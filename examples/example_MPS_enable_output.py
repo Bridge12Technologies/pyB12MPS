70389181e1a77f2387e9ffe4ced51457c4787a6c
Timothy Keller <tkeller@bridge12.com>
2021-09-29 17:56:01 -0400

    Updating examples
    updating enable output example to turn off power after 3 seconds.

--- examples/example_MPS_enable_output.py
+++ examples/example_MPS_enable_output.py
@@ -1,4 +1,5 @@
 import pyB12MPS
+import time
 
 mps = pyB12MPS.MPS()
 
@@ -6,4 +7,10 @@ mps.power(0)
 mps.freq(9.5)
 mps.wgstatus(1)
 mps.rfstatus(1)
+
+time.sleep(3)
+
+mps.rfstatus(0)
+mps.wgstatus(0)
+
 mps.close()
