dcac1e499bf34a9db8d2cf147500c76bc9a1699b
Timothy Keller <tkeller@bridge12.com>
2022-08-12 12:45:25 -0400

    Adding comments to enable output example

--- examples/example_MPS_enable_output.py
+++ examples/example_MPS_enable_output.py
@@ -1,16 +1,16 @@
 import pyB12MPS
 import time
 
-mps = pyB12MPS.MPS()
+mps = pyB12MPS.MPS() # Initialize MPS class
 
-mps.power(0)
-mps.freq(9.5)
-mps.wgstatus(1)
-mps.rfstatus(1)
+mps.power(0) # Set Power to 0 dBm
+mps.freq(9.5) # Set Frequency to 9.5 GHz
+mps.wgstatus(1) # Enable WG Switch for DNP Mode
+mps.rfstatus(1) # Enable RF Output
 
-time.sleep(3)
+time.sleep(3) # Delay 3 seconds
 
-mps.rfstatus(0)
-mps.wgstatus(0)
+mps.rfstatus(0) # Disable RF Output
+mps.wgstatus(0) # Disable WG Switch (back to EPR mode)
 
-mps.close()
+mps.close() # Close the Connection to MPS
