0ae5135fd10abadfe3e5065e70198f9c94b6b181
Yen-Chun Huang <yhuang@bridge12.com>
2023-12-20 12:26:28 -0500

    Update the way to save signed integer and float to eeprom

--- pyB12MPS/mps.py
+++ pyB12MPS/mps.py
@@ -949,6 +949,19 @@ class MPS:
             writeeeprom(64, 'uint32_t', 9000000) # Set the minimum frequency to eeprom at address 64.
         """
 
+        if data_type in [
+            "int8_t",
+            "int16_t",
+            "int32_t",
+        ]:  # convert signed integer to the unsigned interger
+            bit = ""
+            for c in data_type:
+                if c.isdigit():
+                    bit += c
+
+            data += 2 ** int(bit)
+            data_type = "u" + data_type
+
         if data_type in ["char"]:
             self.send_command("write %i %s %s" % (address, data_type, data), recv=True)
 
@@ -956,16 +969,16 @@ class MPS:
             "bool",
             "boolean",
             "uint8_t",
-            "int8_t",
             "uint16_t",
-            "int16_t",
             "uint32_t",
-            "int32_t",
         ]:
             self.send_command("write %i %s %i" % (address, data_type, data), recv=True)
 
         elif data_type in ["float"]:
-            self.send_command("write %i %s %f" % (address, data_type, data), recv=True)
+            data = int(
+                np.float32(data).view(np.uint32)
+            )  # convert to 4 bytes then to uint32_t to store data in EEPROM
+            self.send_command("write %i 'uint32_t' %i" % (address, data), recv=True)
 
         else:
             raise ValueError("Input argument is invalid.")
